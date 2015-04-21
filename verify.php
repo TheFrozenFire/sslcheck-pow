6b92c82bbc6e909e3521d145c4daf2eac9cc04ea
Justin Martin <frozenfire@thefrozenfire.com>
2015-04-21 10:24:13 -0700

    Check score.

--- verify.php
+++ verify.php
@@ -9,6 +9,14 @@ if($verified === 1) {
     echo 'Yep, they did the work'.PHP_EOL;
     
     $result = json_decode($payload, true);
+    
+    $decent = ($result['score'] >= 80);
+    if($decent) {
+        echo 'Their score is good enough'.PHP_EOL;
+    } else {
+        echo 'Their score sucks'.PHP_EOL;
+    }
+    
     $recent = (time() - $result['time'] < 86400);
     if($recent) {
         echo 'And they did it recently'.PHP_EOL;
